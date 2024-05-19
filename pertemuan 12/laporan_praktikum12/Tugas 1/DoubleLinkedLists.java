85a986d23ce4a2a3bd4f40a24305fc14718a6eb0
evanparisya <144124511+evanparisya@users.noreply.github.com>
2024-05-19 18:28:07 +0700

    update

--- pertemuan 12/laporan_praktikum12/Tugas 1/DoubleLinkedLists.java
+++ pertemuan 12/laporan_praktikum12/Tugas 1/DoubleLinkedLists.java
@@ -17,6 +17,45 @@ public class DoubleLinkedLists {
                 current = current.next;
             }
             Node newNode = new Node(current, item, null);
+            current.next = newNode;
+            size++;
+        }
+        System.out.println("Antrian berhasil ditambahkan");
+    }
+
+    public void removeFirst() throws Exception {
+        Node temp = head;
+        if(isEmpty()) {
+            throw new Exception("Linked List masih kosong, tidak dapat dihapus!");
+        } else if (size == 1) {
+            head = null;
+            size--;
+            // return;
+        } else {
+            head = head.next;
+            head.prev = null;
+            size--;
+        }
+        System.out.println(temp.data.nama + " telah selesai divaksinisasi.");
+        print();
+    }
+
+    public void print() {
+        if(isEmpty()) {
+            System.out.println("Linked lists kosong");
+        } else {
+            Node tmp = head;
+            int i = 0;
+            System.out.println("------------------------------");
+            System.out.println("Daftar Pengantri Vaksin");
+            System.out.println("------------------------------");
+            System.out.println("|No.\t|Nama\t|");
+            while(tmp != null) {
+                System.out.print("|" + tmp.data.no + "\t|" + tmp.data.nama + "\t|\n");
+                tmp = tmp.next;
+                i++;
+            }
+            System.out.println("Sisa Antrian: " + i);
         }
     }
 }
