91a2fba7bbe59a87c7214a1590a84df6d8056fb2
evanparisya <144124511+evanparisya@users.noreply.github.com>
2024-05-20 20:38:59 +0700

    update

--- Kuis 2/Main.java
+++ Kuis 2/Main.java
@@ -1,17 +1,32 @@
 /**
  * Main
  */
-public class Main {
+import java.util.Scanner;
+ public class Main {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("==============================");
+        System.out.println("=========== MotoGP ===========");
+        System.out.println("==============================");
+        System.out.print("Masukkan jumlah Pembalap : ");
+        int jumlahPembalap = sc.nextInt();
+
         DoubleLinkedLists.Klasemen klasemen = new DoubleLinkedLists().new Klasemen();
 
-        klasemen.tambahPembalap(new Node("Fabio Quartararo", "Prancis", "Monster Energy Yamaha MotoGP", 122));
-        klasemen.tambahPembalap(new Node("Francesco Bagnaia", "Italia", "Ducati Lenovo Team", 112));
-        klasemen.tambahPembalap(new Node("Aleix Espargaró", "Spanyol", "Aprilia Racing Team Gresini", 75));
-        klasemen.tambahPembalap(new Node("Joan Mir", "Spanyol", "Team Suzuki Ecstar", 68));
-        klasemen.tambahPembalap(new Node("Enea Bastianini", "Italia", "Gresini Racing MotoGP", 64));
+        for (int i = 0; i < jumlahPembalap; i++) {
+            System.out.print("Masukkan nama pembalap: ");
+            String namaPembalap = sc.next();
+            System.out.print("Masukkan negara pembalap: ");
+            String negaraPembalap = sc.next();
+            System.out.print("Masukkan tim pembalap: ");
+            String timPembalap = sc.next();
+            System.out.print("Masukkan poin pembalap: ");
+             int poinPembalap = sc.nextInt();
 
+            klasemen.tambahPembalap(new Node(namaPembalap, negaraPembalap, timPembalap, poinPembalap));
+        }
         klasemen.urutkanPembalap();
         klasemen.cetakKlasemen();
     }
